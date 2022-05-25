ae1a8a880016734f26571ae967f0f688e99b1f50
UeberallGebannt <paulmertens63@gmail.com>
2022-05-25 07:39:53 +0200

    Initial 1.7.10 Port

---
+++ src/main/java/net/kettlemc/opticheck/OptiCheckMod.java
@@ -0,0 +1,79 @@
+package net.kettlemc.opticheck;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraftforge.common.MinecraftForge;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = OptiCheckMod.MODID, name = OptiCheckMod.NAME, version = OptiCheckMod.VERSION)
+public class OptiCheckMod
+{
+    public static final String MODID = "opticheck";
+    public static final String NAME = "Optifine Checker";
+    public static final String VERSION = "1.7.10-1.0";
+
+    private static Logger logger;
+
+    private boolean alreadyChecked = false;
+    private boolean optifinePresent = false;
+
+    private static OptiCheckScreen screen = new OptiCheckScreen();
+    private static OptiCheckConfig config = new OptiCheckConfig();
+
+    /**
+     * Checks if Optifine is installed (Should only be called once!)
+     * @return boolean - if the class was found
+     */
+    private boolean isOptifineInstalled() {
+        try {
+            Class.forName(config.getValue("class"));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private boolean shouldDisplay() {
+        return !optifinePresent && !alreadyChecked;
+    }
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        this.optifinePresent = isOptifineInstalled();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public void openMainMenu(GuiOpenEvent event) {
+        if (shouldDisplay()) {
+            if (event.gui instanceof GuiMainMenu) {
+                event.gui = screen;
+                this.alreadyChecked = true;
+                return;
+            }
+        }
+    }
+
+    public static Logger getLogger() {
+        return logger;
+    }
+
+    public static OptiCheckConfig getConfig() {
+        return config;
+    }
+}
---
+++ src/main/java/net/kettlemc/opticheck/OptiCheckScreen.java
@@ -0,0 +1,65 @@
+package net.kettlemc.opticheck;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
+
+import java.net.URI;
+
+@SideOnly(Side.CLIENT)
+public class OptiCheckScreen extends GuiScreen {
+
+    @Override
+    public void initGui() {
+        if (OptiCheckMod.getConfig().getValueAsInt("mode") == 0) {
+            this.buttonList.add(new GuiButton(0, this.width / 2 - 154, this.height / 2 + 96, 144, 20, "Continue"));
+        } else {
+            buttonList.clear();
+            this.buttonList.add(new GuiButton(0, this.width / 2 - 154, this.height / 2 + 96, 144, 20, "Quit Game"));
+        }
+        this.buttonList.add(new GuiButton(1, this.width / 2 + 10, this.height / 2 + 96, 144, 20, "Link"));
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        drawDefaultBackground();
+        drawCenteredString(this.fontRendererObj, Utils.color(OptiCheckMod.getConfig().getValue("title")), this.width / 2, this.height / 2 - 100, 0xFFFFFF);
+        Utils.handleGuiText(OptiCheckMod.getConfig().getValue("message"), fontRendererObj, this, this.width, this.height);
+
+        super.drawScreen(mouseX, mouseY, partialTicks);
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton button) {
+        switch (button.id) {
+            // Continue or Quit
+            case 0: {
+                for (GuiButton b : buttonList) {
+                    b.enabled = false;
+                }
+                if (OptiCheckMod.getConfig().getValueAsInt("mode") == 0)
+                    this.mc.displayGuiScreen(null); // Clears the current screen
+                else
+                    this.mc.shutdown(); // Closes the Game
+                break;
+            }
+            // Link
+            case 1: {
+                try {
+                    Utils.openUrl(URI.create(OptiCheckMod.getConfig().getValue("url")));
+                } catch (Exception exception) {
+                    OptiCheckMod.getLogger().error("Couldn't open link!");
+                    exception.printStackTrace();
+                }
+                break;
+            }
+        }
+    }
+
+    @Override
+    protected void keyTyped(char typedChar, int keycode) {
+        // We don't do this here.
+    }
+
+}
