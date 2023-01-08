d927613198fb36ad66232fd7651184fc32d45517
UeberallGebannt <paulmertens63@gmail.com>
2023-01-08 13:56:05 +0100

    Bump version to 1.1.0

--- src/main/java/net/kettlemc/opticheck/OptiCheckMod.java
+++ src/main/java/net/kettlemc/opticheck/OptiCheckMod.java
@@ -17,7 +17,7 @@ public class OptiCheckMod {
 
     public static final String MODID = "opticheck";
     public static final String NAME = "Optifine Checker";
-    public static final String VERSION = "1.7.10-1.0.1";
+    public static final String VERSION = "1.7.10-1.1.0";
 
     private static Logger logger;
 
